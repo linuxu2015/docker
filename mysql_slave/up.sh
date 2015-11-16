58302a5beb9efc328d7bffaecaf9323ebe277505
linuxu2015 <linuxu2015@gmail.com>
2015-11-16 12:41:19 +0800

    del mysql_salve

--- mysql_slave/up.sh
+++
@@ -1,2 +0,0 @@
-docker run -d --net=none --name=tech_mysql02 -v /data/tech_mysql_slave/mysql:/var/lib/mysql -v /data/tech_mysql_slave/log:/var/log/mysql tech/mysql_slave
-pipework vlan121 tech_mysql02 10.121.0.2/24@10.121.0.254
