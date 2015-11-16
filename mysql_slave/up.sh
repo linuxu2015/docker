97ef75351deed6bc7a26135718ccfd377ee59601
linuxu2015 <linuxu2015@gmail.com>
2015-11-16 20:42:26 +0800

    nothing

--- mysql_slave/up.sh
+++ mysql_slave/up.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 docker run -d --net=none --name=tech_mysql02 -v /data/tech_mysql_slave/mysql:/var/lib/mysql -v /data/tech_mysql_slave/log:/var/log/mysql tech/mysql_slave
 pipework vlan121 tech_mysql02 10.121.0.2/24@10.121.0.254
